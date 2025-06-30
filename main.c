6a8108fad4dd95823c377a93dc00c8fe1f6ba91b
GregTash <Gregtash4@gmail.com>
2025-06-30 18:51:41 +1000

    Added reading from a file

--- main.c
+++ main.c
@@ -1,8 +1,25 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 
 int main(int argc, char *argv[]) {
-    FILE *file = fopen("text.txt", "r"); 
+    FILE *file = fopen("text.txt", "a+"); 
+
+    if (file == NULL) {
+        printf("Could not open file!\n");
+    } else {
+        printf("File opened!\n");
+    }
+
+    fseek(file, 0, SEEK_END);
+    long length = ftell(file);
+    fseek(file, 0, SEEK_SET);
+    char *buffer = malloc(length);
+
+    if (buffer) {
+        fread(buffer, sizeof(char), length, file);
+    }
+
+    printf("%s", buffer);
 
     fclose(file);
     
